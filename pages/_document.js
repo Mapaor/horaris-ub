9bbeaaa81ce46a588c1fc486e823f9ac7f8a5513
Mapaor <135515260+Mapaor@users.noreply.github.com>
2025-05-30 15:41:00 +0200

    added title

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default function Document() {
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
                 />
                 <link rel="icon" href="/favicon.ico" />
+                <title>Horaris Física UB</title>
                 <meta property="og:title" content="Horaris Física UB" />
                 <meta property="og:description" content="Versió alternativa a la Guia Acadèmica de Física UB per consultar horaris, plans docents i altra informació sobre les assignatures del grau." />
                 <meta property="og:image" content="/horaris-ub.jpg" />
